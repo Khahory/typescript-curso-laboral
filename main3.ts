02dda36c8cc657b8982e6c08cf5c76ff6e95d28b
angelmaria <angeld05@live.com>
2019-08-29 09:42:56 -0400

    Commit 3

---
+++ main3.ts
@@ -0,0 +1,6 @@
+let batman:string = "Batman";
+let spiderMan:string = "Spider Man";
+let joker:string = "Joker";
+
+let cadena:string = `Esto son los heroes? ${batman} y ${spiderMan}?, alguien que me hable de ${joker}`;
+console.log(cadena);
