55c820fa31ce51231d2fe0660c97634162eaea9a
Khahory <angeld05@live.com>
2019-08-31 18:05:01 -0400

    Creando funciones genericas (Extenso)

--- main.ts
+++ main.ts
@@ -1,10 +1,31 @@
 
 //Creando funciones genericas (Para que puedan reconocer de que tipo de dato es)
 
+
 function regresar<T>(arg:T) {
     return arg;
 }
 
-console.log(  regresar("Hola amigos").charAt(2)  );
-console.log(  regresar(10.12421).toFixed(2)  );
-console.log(  regresar(new Date()).getTime()  );
+type Hero = {
+    nombre:string,
+    alias:string
+}
+
+type Villano = {
+    muertes:number,
+    odio:string
+}
+
+let deadpool = {
+    nombre: "DeadPool",
+    nombreReal: "Wade",
+    poder: "Regeneracion",
+    odio: "Mucho",
+    alias: "Cabron",
+    muertes: 100
+}
+
+console.log(    regresar<Hero>(deadpool).alias  );
+console.log(    regresar<Villano>(deadpool).odio  );
+//Dependiendo del tipo que sea pues mostrara sus metodos
+//correspondientes
