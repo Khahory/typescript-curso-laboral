7d625a5c992ec97d5ea1525e1110f3382cc78e92
angelmaria <angeld05@live.com>
2019-09-02 10:59:01 -0400

    Preparando parte 2

--- Validacion/numeros.ts
+++
@@ -1,4 +0,0 @@
-export const PI = 3.14155;
-export default function restar(num1: number, num2: number):number {
-    return num1 - num2;
-}
--- Validacion/textos.ts
+++
@@ -1,8 +0,0 @@
- const MENSAJES:string[] = [
-    "El texto es muy corto",
-    "El texto es muy largo"
-];
-
-export function obtenerError(numError:number):string  {
-    return MENSAJES[numError];
-}
