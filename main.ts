7daecc89376d5eac88ee8148eaeb7f20f697b3b7
angelmaria <angeld05@live.com>
2019-09-02 13:07:15 -0400

    Aplicando jquery

--- main.ts
+++ main.ts
@@ -1,10 +1,21 @@
-console.log("Hola mundo");
 
+
+console.log("Hola mundo");
 $(document).ready(function () {
     console.log("Pagina cargada y lista...");
 });
 
 $('#botAlerta').on("click", function () {
-    $("h1").text("Hola desde typescript");
+    $("h1")
+
+        .text("Hola desde typescript");
+
+    // Swal.fire(
+    //     'Good job!',
+    //     'You clicked the button!',
+    //     'success'
+    // )
 });
+
+
 
