713d17ee13d13ae1316307744f6b72b63d87802b
angelmaria <angeld05@live.com>
2019-09-02 10:20:49 -0400

    Decoradores metodos (error, documentacion cambio)

--- main.ts
+++ main.ts
@@ -1,3 +1,29 @@
+//Crear nuestro decorador para que las funciones no se puedan sobreescribir con booleano
+function editable(esEditable:boolean){
+    return function (target: any, nombrePropiedad: string, descriptor: PropertyDescriptor) {
+        descriptor.writable = esEditable;
+    }
+}
+
+class Hero {
+    constructor(public nombre:string){};
+
+    @editable(false)
+    plan(){
+        console.log("Salvar al mundo");
+    }
+}
+
+let spiderMan = new Hero("Peter");
+spiderMan.plan = function () {
+    console.log("Destruir el mundo");
+}
+spiderMan.plan();
+
+
+
+
+console.error("siguiente");
 //Decorador clase normal
 function cinsola(constructor: Function) {
     console.log(constructor); //Aqui podemos usar los parametros que tiene el constructor (this.nombre en prototype)
@@ -12,7 +38,7 @@ function imprimirConsola(imprimir:boolean):Function {
     }
 }
 
-@imprimirConsola(false)
+@imprimirConsola(true)
 class Villano {
     constructor(public nombre:string){}
 }
